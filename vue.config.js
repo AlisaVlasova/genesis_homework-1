baa3eb48c2a028baa79a9c0649a4ff33a54bce78
Alisa Vlasova <alisavlasova@MacBook-Pro.local>
2022-02-05 17:29:38 +0200

    esbuild-loader implemented

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,38 @@
+const { ESBuildMinifyPlugin } = require("esbuild-loader");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const esbuild = require("esbuild");
+
 module.exports = {
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new ESBuildMinifyPlugin({
+          target: "es2015",
+          css: true,
+        }),
+      ],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "esbuild-loader",
+          options: {
+            target: "es2015",
+            implementation: esbuild,
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+      ],
+    },
+
+    plugins: [new MiniCssExtractPlugin()],
+  },
+
   lintOnSave: false,
 
   transpileDependencies: ["vuetify"],
